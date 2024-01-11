6f775184dbf1c28d3b790f64c5c920b0da28d69b
Abdi-01 <abdialghi@gmail.com>
2024-01-11 20:19:30 +0700

    feat: regis user API

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,20 @@
 import { Request, Response, NextFunction } from "express";
+import prisma from "../prisma";
 
 export class AuthController {
     async registerUser(req: Request, res: Response, next: NextFunction) {
         try {
-            console.log(req.body);
+            const { username, email, password } = req.body;
 
+            const newUser = await prisma.user.create({
+                data: {
+                    username,
+                    email,
+                    password
+                }
+            });
+
+            res.status(201).send(newUser);
         } catch (error: any) {
             console.log(error);
             return res.status(500).send(error);
