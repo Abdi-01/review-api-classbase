fe4671a534aa2c8b99a8ca18e4b243aec56659a6
Abdi-01 <abdialghi@gmail.com>
2024-01-15 20:40:42 +0700

    fix: upload multiple file

--- src/controllers/sample.controller.ts
+++ src/controllers/sample.controller.ts
@@ -13,6 +13,18 @@ export class SampleController {
         try {
             console.log(req.file);
 
+            return res.status(200).send(`file ${req.file?.filename} successfully uploaded`)
+        } catch (error: any) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+    }
+
+    async addMultipleImage(req: Request, res: Response) {
+        try {
+            console.log(req.files);
+
+            return res.status(200).send("Upload multiple files success")
         } catch (error: any) {
             console.log(error);
             return res.status(500).send(error);
--- src/routers/sample.router.ts
+++ src/routers/sample.router.ts
@@ -16,6 +16,7 @@ export class SampleRouter {
         this.router.get("/", this.sampleController.getSample);
         this.router.post("/", this.sampleController.createSample);
         this.router.post("/upload", uploader("IMG", "/image").single("gambar"), this.sampleController.addNewImage);
+        this.router.post("/multiple-upload", uploader("IMG", "/image").array("gambar", 3), this.sampleController.addMultipleImage);
     }
 
     getRouter(): Router {
